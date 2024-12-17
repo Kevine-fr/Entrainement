f9f6975a5d3ed6e541ab7a869a21c6e2c42386df
Kevine-fr <124605694+Kevine-fr@users.noreply.github.com>
2024-12-17 03:02:23 +0100

    Update welcome.blade.php
    Signed-off-by: Kevine-fr <124605694+Kevine-fr@users.noreply.github.com>

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,12 +23,12 @@
                     <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
                 @else
                     @php
-                        // Vérifie si la table Users est vide ou non
-                        $userCount = DB::table('users')->count();
+                        // Vérifie si la table Users contient des utilisateurs
+                        $userExists = DB::table('users')->exists();
                     @endphp
 
                     {{-- Si la table Users est vide, le bouton d'inscription est caché --}}
-                    @if ($userCount == 0)
+                    @if (!$userExists)
                         <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
                     @endif
 
