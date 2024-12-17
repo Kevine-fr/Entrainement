0ba09ad395b9cd5143ab9964d4aa43c802cbbc86
Kevine-fr <124605694+Kevine-fr@users.noreply.github.com>
2024-12-17 01:48:18 +0100

    Update docker-entrypoint.sh
    Signed-off-by: Kevine-fr <124605694+Kevine-fr@users.noreply.github.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 
-# Ajouter node_modules/.bin au PATH
-# export PATH=/var/www/html/node_modules/.bin:$PATH
-
 # Installer les dépendances Node.js (au cas où elles n'auraient pas été installées)
 npm install
 
 # Lancer le serveur PHP en arrière-plan
 php artisan serve --host=0.0.0.0 --port=8000 &
 
-# Lancer les tâches npm
+# Lancer les tâches npm (par exemple, le développement du frontend)
 npm run dev &
 
 # Garder le conteneur en cours d'exécution
