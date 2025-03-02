bfbe67cb072314e365a69fceb5b5473f93a1df79
Kevine-fr <124605694+Kevine-fr@users.noreply.github.com>
2025-03-02 14:43:38 +0100

    Update login.blade.php
    Signed-off-by: Kevine-fr <124605694+Kevine-fr@users.noreply.github.com>

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,7 +12,6 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        <script type="module" src="http://[::1]:5173/@vite/client"></script><link rel="stylesheet" href="http://[::1]:5173/resources/css/app.css" /><script type="module" src="http://[::1]:5173/resources/js/app.js"></script>    </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div>
